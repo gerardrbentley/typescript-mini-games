3f9ce53775741f4f4f2c1e9529f4a3491e88d83d
Gerard Bentley <gerardrbentley@gmail.com>
2020-11-12 08:59:39 -0500

    Update base game folder with helper querySelectors

--- base/app.js
+++ base/app.js
@@ -1,1 +1,7 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const allSquares = [];
+    const scoreDisplay = document.querySelector('#current-score');
+    const resetButton = document.querySelector('#reset-button');
+});
---
+++ base/app.ts
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const allSquares: Array<HTMLDivElement> = [];
+    const scoreDisplay = document.querySelector('#current-score');
+    const resetButton = document.querySelector('#reset-button');
+})
